a8573b3de02e69466414226ce63025bf722a6e22
DESKTOP-OERL80K\PC <nguyenvanthe1562000@gmail.com>
2022-01-06 17:58:47 +0700

    the push

--- ManagerChannel/Models/Project/Project.cs
+++ ManagerChannel/Models/Project/Project.cs
@@ -1,6 +1,25 @@
-﻿namespace ManagerChannel.Models.Project
+﻿using API.Models;
+using API.Models.Authorization;
+using System;
+
+namespace ManagerChannel.Models.Project
 {
-    public class Project
+    public class Project : BaseModel, ISoftDeletableModel, ILoggableUserActionModel
     {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Note { get; set; }
+        public DateTime StartDate { get; set; }
+
+        //------------------------------------------
+        public string CreatedByUserId { get; set; }
+        public User CreatedByUser { get; set; }
+        public string UpdatedByUserId { get; set; }
+        public User UpdatedByUser { get; set; }
+        public string DeletedByUserId { get; set; }
+        public User DeletedByUser { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDate { get; set; }
     }
 }
