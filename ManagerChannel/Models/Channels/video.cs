4c7cd8350d1ee1d0ec7ea4fadf73fc619bce4389
khainguyen143 <khainguyen143@gmail.com>
2022-01-06 17:59:19 +0700

    khai push

---
+++ ManagerChannel/Models/Channels/video.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+using API.Models.Authorization;
+using System;
+
+namespace ManagerChannel.Models.Channels
+{
+    public class Video : BaseModel, ILoggableUserActionModel, ISoftDeletableModel
+    {
+        public string VideoId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? PublicDate { get; set; }
+        public bool IsClamed { get; set; }
+        public bool IsDeletedByChannelManager { get; set; }
+        public string Thumnail
+
+
+        public string ChannelId { get; set; }
+        public YoutubeChannel Channel { get; set; }
+        //-----------------------------------------
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public string CreatedByUserId { get; set; }
+        public User CreatedByUser { get; set; }
+        public string UpdatedByUserId { get; set; }
+        public User UpdatedByUser { get; set; }
+        public string DeletedByUserId { get; set; }
+        public User DeletedByUser { get; set; }
+    }
+}
---
+++ ManagerChannel/Models/Pay/PaymentAccount.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+using API.Models.Authorization;
+using ManagerChannel.Models.Channels;
+using ManagerChannel.Models.Networks;
+using System;
+
+namespace ManagerChannel.Models.Pay
+{
+    public class PaymentAccount : BaseModel, ILoggableUserActionModel, ISoftDeletableModel
+    {
+        public string Name { get; set; }
+        public string Note { get; set; }
+        
+        //-----------------------------------------
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public string CreatedByUserId { get; set; }
+        public User CreatedByUser { get; set; }
+        public string UpdatedByUserId { get; set; }
+        public User UpdatedByUser { get; set; }
+        public string DeletedByUserId { get; set; }
+        public User DeletedByUser { get; set; }
+    }
+}
